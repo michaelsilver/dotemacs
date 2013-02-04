5e717ad05af4702d502fd95523e1f6565474397b
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-02-04 10:55:57 +0200

    current way of configuring yasnippet

--- fd-yasnippet.el
+++ fd-yasnippet.el
@@ -1,11 +1,6 @@
-
 ;; YASnippet
 (require 'yasnippet)
 (setq yas-snippet-dirs '("~/.emacs.d/el-get/yasnippet/snippets/" "~/.emacs.d/my-snippets/"))
-(yas--initialize)
-
-(dolist (sdir yas-snippet-dirs)
-  (when (file-accessible-directory-p sdir)
-    (yas/load-directory sdir)))
+(yas-global-mode 1)
 
 (provide 'fd-yasnippet)
