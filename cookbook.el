9dc0c3224c21556858303849751b7706c26e5231
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-03-06 13:06:12 +0200

    Added a cookbook

---
+++ cookbook.el
@@ -0,0 +1,22 @@
+;; This is a cookbook I will fill with random elisp I write and might
+;; need in the future but dont want loaded with emacs.
+
+;; Helpers for sorting grep results
+(defun file-size (filename)
+  (nth 7 (file-attributes filename)))
+
+(defun grep-current-line-filesize ()
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (let ((p1 (point)))
+      (search-forward ":") (backward-char)
+      (let ((fsize (file-size (buffer-substring p1 (point)))))
+	(beginning-of-line)
+	(insert (format "filesize:%d| " fsize))))))
+
+(defun remove-grep-line-filesize ()
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (replace-regexp "^filesize[0-9]+| " "")))
