ba79714ff6706eeff8c10f50e9d4942e40bcbe2e
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-12-29 22:11:07 +0200

    Tab removed, o0nly s-tab from now on

--- fd-yasnippet.el
+++ fd-yasnippet.el
@@ -5,5 +5,7 @@
 
 (global-set-key (kbd "C-h y") 'yas-describe-tables)
 (global-set-key (kbd "<backtab>") 'yas-expand) ; S-Tab
+(define-key yas-minor-mode-map [(tab)] nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
 
 (provide 'fd-yasnippet)
