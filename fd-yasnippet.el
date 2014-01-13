c4911b7c3a0d6621ff1d30ff33340dbf8b4e0d9f
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-01-13 11:50:24 +0200

    fd-prev-match to regex match in context above the point. Very useful in context aware snippets like super.

--- fd-yasnippet.el
+++ fd-yasnippet.el
@@ -1,4 +1,12 @@
 ;; YASnippet
+
+(defun fd-prev-match (rx alt &optional grp)
+  "Get the last occurance or alt. GRP tells us which rx group to
+keep, defaults to 1."
+  (or
+   (progn (re-search-backward rx nil t) (match-string-no-properties (or grp 1)))
+   alt))
+
 (require 'yasnippet)
 (add-to-list 'yas-snippet-dirs (my-expand-path "my-snippets/"))
 (yas-global-mode 1)
