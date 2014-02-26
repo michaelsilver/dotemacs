7ce2e7f232c51fa8ad1c3ac30a71ab5c3e4fdf73
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-02-26 11:30:31 +0200

    *.vimp is for Vimperator

--- fd-automode.el
+++ fd-automode.el
@@ -14,5 +14,6 @@
 (add-to-list 'auto-mode-alist '("[.]js" . js2-mode))
 (add-to-list 'auto-mode-alist '("[.]jsm" . js2-mode))
 (add-to-list 'auto-mode-alist '("[.]java" . java-mode))
+(add-to-list 'auto-mode-alist '("[.]vimp" . vimperator-mode))
 
 (provide 'fd-automode)
