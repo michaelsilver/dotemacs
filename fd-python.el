c9e9e12b4d975abec68a7d5aed81d3aeeac43574
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-04-25 19:12:36 +0300

    Python mainly

--- fd-python.el
+++ fd-python.el
@@ -25,6 +25,7 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
+(setq python-shell-virtualenv-path "~/bin/py")
 
 (defun fd-venv-path (path)
   (interactive (list (ido-read-directory-name "Virtualenv root: ")))
