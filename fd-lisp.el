d729ac7e2dee63eb0929592767e4b199a9d357f3
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-06-14 16:04:23 +0300

    Clojure tags generation.

--- fd-lisp.el
+++ fd-lisp.el
@@ -19,7 +19,19 @@
 ;; Slime
 (require 'clojure-mode)
 (require 'cider)
+
+(defvar fd-clojure-etags-gen-command "find . \! -name '.*' -name '*.clj' \
+    | xargs etags \
+    --regex='/[ \t\(]*def[a-zA-Z!$%&*+\-.\/:<=>?@^_~]*[ \n\t]+\(\^{[^}]*}[ \n\t]+\|\)\([a-zA-Z!$%&*+\-.\/:<=>?@^_~]+\)/\2/s' \
+    --regex='/[ \t\(]*ns \([a-z.]+\)/\1/'"
+  "Found see here http://stackoverflow.com/questions/1481842/clojure-emacs-etags#6726061")
+
+(defun fd-clojure-mode-hook ()
+  "Hooks for all clojure."
+  (setq-local etags-table-create-table-command fd-clojure-etags-gen-command))
+
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'clojure-mode-hook 'fd-clojure-mode-hook)
 
 (require 'ac-nrepl)
 (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
