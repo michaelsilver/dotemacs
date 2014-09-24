b7ea1bc6b0ab86b517271b31d1417908a2c3405a
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-09-24 15:58:45 +0300

    Compilation mode support for node

--- fd-javascript.el
+++ fd-javascript.el
@@ -1,6 +1,11 @@
 (require 'js2-mode)
 (require 'slime-js)
-(setq slime-js-swank-command "/usr/sbin/npm")
+(setq slime-js-swank-command
+      (concat (replace-in-string
+	       (shell-command-to-string "/usr/sbin/npm bin -g 2> /dev/null")
+	       "\n" "")
+	      "/swank-js")
+      slime-js-swank-args nil)
 
 (defvar fd-js-etags-gen-command (replace-in-string
 				 "find . \! -name '.*' -regex '.*\.jsm?' |
@@ -14,13 +19,32 @@ xargs etags --language=none
   properties that are preceeded by spaces and top level
   variables")
 
+(defun js-jack-in-browser ()
+  (interactive)
+  (unless (get-process "swank-js")
+    (slime-js-run-swank))
+
+  (slime-connect "localhost" 4005 nil t)
+  (slime-repl))
 
 (defun fd-js-mode-hook ()
   "Hooks for all clojure."
   (slime-js-minor-mode 1)
+  (setq tab-width 2
+	js2-basic-offset 2)
   (setq-local etags-table-create-table-command-string
 	      fd-js-etags-gen-command))
 
+(defvar js-compilation-error-regex-alist
+  ;; REGEX FILE-GROUP LINE-GROUP COLUMN-GROUP ERROR-TYPE LINK-GROUP
+  '(nodejs
+    "^[ \\t]*at.*(\\(\\(.*\\):\\([0-9]*\\):\\([0-9]*\\)\\))$"
+    2 3 4 nil 1))
+
+(add-to-list 'compilation-error-regexp-alist
+	     (car js-compilation-error-regex-alist))
+(add-to-list 'compilation-error-regexp-alist-alist
+	     js-compilation-error-regex-alist)
 
 (add-hook 'js2-mode-hook 'fd-js-mode-hook)
 
